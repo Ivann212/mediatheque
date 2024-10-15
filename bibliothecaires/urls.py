1b28508b059e78f9a7947d67b9644f4400002650
Ivann <212ivann.rambaud@gmail.com>
2024-10-15 21:33:24 +0200

    modification de la page d'acceuil de l'app bibliothécaires

--- bibliothecaires/urls.py
+++ bibliothecaires/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.listemedias, name='listemedias'),
+    path('',auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
     path('ajoutmedia/', views.ajoutmedia, name='ajoutmedia'),
     path('deletemedia/<str:nom>/', views.deletemedia, name='deletemedia'),
     path('updatemedia/<str:nom>/', views.updatemedia, name='updatemedia'),
@@ -16,6 +16,6 @@ urlpatterns = [
     path('emprunts/', views.liste_emprunts, name='liste_emprunts'),
     path('creer_emprunt/', views.creer_emprunt, name='creer_emprunt'),
     path('retourner_media/', views.retourner_media, name='retourner_media'),
-    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('listemedias/', views.listemedias, name='listemedias'),
     path('logout/', views.custom_logout, name='logout'),
 ]
